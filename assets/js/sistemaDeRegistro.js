82dbe592c6c9b0e45177fa0fc618863c7c4ab58d
AntonioTrrs <antonio1959988@hotmail.com>
2023-09-26 18:05:49 -0600

    correccion de errores

--- assets/js/sistemaDeRegistro.js
+++ assets/js/sistemaDeRegistro.js
@@ -37,7 +37,7 @@ function registrarPaciente(){
     mostrarInfoPaciente(paciente); // Invocacion de otra funcion para mostrar la info del paciente
 
     // 2. Muestro el Mensaje de confirmacion (textContent)
-    mensajeConfirmacion.textContent = "<p>Paciente registrado con exito!</p>";
+    mensajeConfirmacion.textContent = "Paciente registrado con exito";
 
     // 3. Genera la tarjeta del paciente
     generarTarjetaPaciente(paciente); // Invocacion de una funcion para generar tarjetas
